69213b730b309ed2e4aa0f9c0102674011d33ba0
Paula Palermo <paulammaira@gmail.com>
2018-06-27 11:20:29 -0300

    Primeira aula de REACT, base para começo

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div id ="cabeçalho">
+      <header>
+        <h1>OIE</h1>
+        <h2 className = "azul"> lokona</h2>
+        </header>
+        <p>zisfaisjdojn askdjnasd napohjoqj skmasljkdn jxnasjdnq ejsdoiwejasda ejwenaksjdan dajsdnoJANDNNAO ma=d na medmsmamortonsiroienonsdfoinweon </p>
+        <p>logique que noan´sakdjn</p>
+      </div>
+    );
+  }
+}
+
+export default App;
